de6998fd5dea10e3524ecbcbba079f5fcf30cae5
Andrew <andrewjsusanto@gmail.com>
2024-04-21 20:31:29 -0700

    Finished scene

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 //import * as THREE from '../node_modules/three/build/three.module.js';
 import * as THREE from 'three';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
 function main() {
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer( {antialias: true, canvas} );
 
-    const fov = 75;
+    const fov = 45;
     const aspect = 2;
     const near = 0.1;
-    const far = 5;
+    const far = 40;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
-    camera.position.z = 2;
+    //camera.position.z = 3;
+    camera.position.set( 0, 3, 15 );
     const scene = new THREE.Scene();
 
     // LIGHT SOURCE
@@ -35,13 +36,60 @@ function main() {
         new THREE.MeshBasicMaterial( {map: loadColorTexture( '../resources/images/flower-6.jpg')} ),
     ]
 
+    // LANDSCAPE
+    const planeSize = 20;
+    const checker_texture = loader.load('../resources/images/checker.png');
+    checker_texture.colorSpace = THREE.SRGBColorSpace;
+    checker_texture.wrapS = THREE.RepeatWrapping;
+    checker_texture.wrapT = THREE.RepeatWrapping;
+    checker_texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    checker_texture.repeat.set( repeats, repeats  );
+
+    const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
+    const planeMat = new THREE.MeshPhongMaterial( {
+        map: checker_texture,
+        side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh( planeGeo, planeMat );
+    mesh.rotation.x = Math.PI * -.5;
+    scene.add( mesh );
+
+    // CUSTOM 3D OBJ
+    const objLoader = new OBJLoader();
+    objLoader.load('../resources/models/bench/Bench.obj', (root) => {
+        root.scale.setScalar(10);
+        root.position.set(2.5, 0, -2.2)
+        root.rotation.y = 2.7;
+        root.traverse(node => {
+            const material = new THREE.MeshPhongMaterial( {
+                color: 0x964B00,
+            })
+            if (material) {
+                node.material = material;
+            }
+        }) 
+        scene.add(root);
+        // add material textures
+    })
+
     // SPHERE
     // radius, height segments, width segments
-    const sphere_geometry = new THREE.SphereGeometry(0.5, 32, 16);
+    const sphere_texture = loader.load('../resources/images/sunrays.jpg');
+    sphere_texture.colorSpace = THREE.SRGBColorSpace;
+    sphere_texture.wrapS = THREE.RepeatWrapping;
+    sphere_texture.wrapT = THREE.RepeatWrapping;
+    sphere_texture.magFilter = THREE.NearestFilter;
+    const sphere_geometry = new THREE.SphereGeometry(1.5, 32, 16);
+    const sphere_color = new THREE.Color(0xFFFF00)
     // MeshStandardMaterial has some gloss for lightsource feedback
-    const sphere_material = new THREE.MeshStandardMaterial( { color: 0xFFFFFF, roughness: 0} );
+    const sphere_material = new THREE.MeshStandardMaterial( {
+        map: sphere_texture,
+        color: 0xFFFFFF,
+        roughness: 0} );
     const sphere = new THREE.Mesh( sphere_geometry, sphere_material );
-    sphere.position.x = 1.5
+    sphere.material.color = sphere_color;
+    sphere.position.set(-5, 6, -3);
     scene.add(sphere);
 
     function animate(time) {
@@ -57,28 +105,28 @@ function main() {
     requestAnimationFrame(animate);
 
     // CYLINDER
-    const cyl_geometry = new THREE.CylinderGeometry(0.4, 0.4, 0.8, 16);
+    const cyl_geometry = new THREE.CylinderGeometry(0.25, 0.5, 2, 16);
     const cyl_material = new THREE.MeshPhongMaterial( {color: 0x44aa88} );
     const cylinder = new THREE.Mesh( cyl_geometry, cyl_material);
-    cylinder.position.x = -1.5;
+    cylinder.position.set(6, 1, 1);
     scene.add(cylinder);
 
-    function cyl_animate(time) {
-        time *= 0.001;
+    // function cyl_animate(time) {
+    //     time *= 0.001;
 
-        requestAnimationFrame(cyl_animate);
-        cylinder.rotation.x = time;
+    //     requestAnimationFrame(cyl_animate);
+    //     cylinder.rotation.x = time;
 
-        renderer.render(scene, camera);
-    }
-    requestAnimationFrame(cyl_animate);
+    //     renderer.render(scene, camera);
+    // }
+    //requestAnimationFrame(cyl_animate);
 
 
 
     // CUBE
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
+    const boxWidth = 1.25;
+    const boxHeight = 1.25;
+    const boxDepth = 1.25;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
     //const material = new THREE.MeshPhongMaterial( {color: 0x44aa88} );
@@ -88,8 +136,10 @@ function main() {
     // Cube rendering with multiple texture load wait
     loadManager.onLoad = () => {
         const cube = new THREE.Mesh(geometry, materials);
+        cube.position.set(6, 2, 1);
         scene.add(cube);
         cubes.push(cube);
+        
     };
 
     function render(time) {
@@ -99,7 +149,7 @@ function main() {
             const speed = 1 + ndx * .1;
             const rot = time * speed;
             cube.rotation.x = rot;
-            cube.rotation.y = rot;
+            cube.rotation.y = rot / 1.5;
         })
     
         renderer.render(scene, camera);
